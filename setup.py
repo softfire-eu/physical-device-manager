cea8d2020c83b9784ba6adc924a32ea02ffc4e4e
Lorenzo Tomasini <lorenzo.tomasini@gmail.com>
2017-06-01 20:35:36 +0200

    updated version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="physical-device-manager",
-    version="0.1.1",
+    version="0.1.2",
     author="SoftFIRE",
     author_email="softfire@softfire.eu",
     description="The SoftFIRE Physical Device Manager",
